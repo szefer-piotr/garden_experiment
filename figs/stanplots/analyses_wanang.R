9e6093bf3d1427a12caab43276c0cbfa6da4fc2a
Piotr Szefer <szefer85@gmail.com>
2019-01-07 19:35:36 +0100

    :scroll: biomass of individuals and leaf damage

--- figs/stanplots/analyses_wanang.R
+++ figs/stanplots/analyses_wanang.R
@@ -7,21 +7,26 @@
 # load data
 AllTestData <- read.table("figs/stanplots/all_test_data.txt")
 
+treat_levels = c("CONTROL", 
+                 "FUNGICIDE",
+                 "INSECTICIDE", 
+                 "PREDATOR",
+                 "WEEVIL25", 
+                 "WEEVIL125")
+
 AllTestData$TREAT = factor(AllTestData$TREAT,
-                           levels = c("CONTROL", 
-                                      "FUNGICIDE",
-                                      "INSECTICIDE", 
-                                      "PREDATOR",
-                                      "WEEVIL25", 
-                                      "WEEVIL125"))
+                           levels = treat_levels)
 
 TreeTestDataset <- read.table("figs/stanplots/tree_test_data.txt")
+TreeTestDataset$TREATMENT = factor(TreeTestDataset$TREATMENT,
+                           levels = treat_levels)
 
 # Individual based biomass and trait data
 inddat <- read.table("datasets/inddataWanang.txt")
 inddat$LIFE.FORM <-as.character(inddat$LIFE.FORM)
 indtree <- inddat[inddat$LIFE.FORM %in% c("shrub","tree"), ]
 
+indtree$TREAT = factor(indtree$TREAT, levels = treat_levels)
 
 # Load packages
 library(brms)
@@ -29,6 +34,9 @@ library(ggplot2)
 library(bayesplot)
 library(tidybayes)
 library(dplyr)
+library(ggridges)
+
+library(stringr)
 
 # plots of the raw data ---------------------------------------------------
 
@@ -108,7 +116,7 @@ marginal_effects(l1W_ranef_lnorm)
 
 #the model above still has a hard time with divergent iterations. I am curious
 #to try how it works if the variables are coded by hand to develop contrasts
-library(stringr)
+
 
 AllTestData_recode <- AllTestData %>% 
   #remove the weevil stuff just for now
@@ -222,7 +230,6 @@ pp + theme_bw()
 
 
 # plots
-library(ggridges)
 
 # Old plot
 # AllTestData %>% 
@@ -236,11 +243,7 @@ library(ggridges)
 # Included in the suplementary and determining "significance"
 sph <- stanplot(l1W_ranef, type = "hist", pars="^b_")
 
-
-# Change the name of this
-sph$data$Parameter
-
-#png("figs/bio_all_diff.png")
+png("figs/bio_all_diff.png")
 
 par(mfrow = c(3,2))
 
@@ -261,7 +264,7 @@ for(type in unique(sph$data$Parameter)){
   
 }
 
-#dev.off()
+dev.off()
 
 # Colours based on posterior distributions of differences between
 # control and a treatment
@@ -324,26 +327,136 @@ AllTestData %>%
 # Specify the equation
 # UNDER CONSTRUCTION - DON'T EVEN KNOW IF THIS IS OK
 
-bio_lnorm_ind <- bf(WEIGHT ~ 1 + TREAT + (1 + TREAT|BLOCK) + (1|SP_CODE), family = lognormal())
+bio_gaus_ind <- bf(WEIGHT ~ 1 + TREAT + (1 + TREAT|BLOCK) + (1 + TREAT|SP_CODE),
+                   family = "lognormal")
 
-bio_rand_ind <- brm(bio_lnorm_ind,
+## Set some priors 
+priors <- c(set_prior("normal(4, 2)", class = "Intercept"),
+            set_prior("normal(0, 3)", class = "b"),
+            set_prior("normal(0, 4)", class = "sd"),
+            set_prior("normal(0, 3)", class = "sigma"),
+            set_prior("lkj(2)", class = "cor"))
+
+bio_rand_ind <- brm(bio_gaus_ind,
                     data=indtree,
                     control = list(adapt_delta = .8))
 
 summary(bio_rand_ind)
 
+sph <- stanplot(bio_rand_ind, type = "hist", pars="^b_")
+
+png("figs/bio_ind_tree_diff.png")
+#X11(width=10, height=10)
+par(mfrow = c(3,2))
+for(type in unique(sph$data$Parameter)){
+  
+  vs <- sph$data$value[sph$data$Parameter == type]
+  
+  colour = "lightblue"
+  
+  if (mean(vs<0) > 0.90) colour = "orange"
+  if (mean(vs<0) > 0.95) colour = "red"
+  
+  hist(vs, breaks = 50,
+       main = paste(type, " ", round(mean(vs<0)*100,1), " %"),
+       xlab="Difference", col = colour)
+  
+  abline(v = 0, lty=2)
+  
+}
+dev.off()
+
+colour = as.character(indtree$TREAT)
+colour[colour == "CONTROL"] <- "grey50"
+colour[colour == "FUNGICIDE"] <- "grey50"
+colour[colour == "INSECTICIDE"] <- "grey50"
+colour[colour == "PREDATOR"] <- "grey50"
+colour[colour == "WEEVIL25"] <- "red"
+colour[colour == "WEEVIL125"] <- "red"
+indtree$colour = colour
+
+png("figs/bio_ind_bayes_hist.png")
 indtree %>% 
   tidybayes::add_predicted_draws(bio_rand_ind, n = 1000) %>%
-  ggplot(aes(x = .prediction, y = TREAT, group = TREAT)) + 
+  ggplot(aes(x = log(.prediction), y = TREAT, group = TREAT, fill=colour)) + 
   geom_density_ridges(alpha = 0.5) + 
-  scale_fill_manual(values = c("grey", "orange", "red")) +
-  geom_point(aes(x = log(BIO), y = TREAT), size = 3, alpha = 0.1,
-             color = "grey50") + 
+  scale_fill_manual(values = c("grey", "red","orange")) +
+  #geom_point(aes(x = log(WEIGHT), y = TREAT), size = 3, alpha = 0.1,
+  #           color = "grey50") + 
   theme_bw()
+dev.off()
+
+# 1c. leaf damage ----------------------
 
 
+# Leaf damage logit values
+ld_gaus_ind <- bf(HERB ~ 1 + TREAT + (1 + TREAT|BLOCK) + (1 + TREAT|SP_CODE))
+
+ld_prior <- c(set_prior("normal(-10, 4)", class = "Intercept"),
+              set_prior("normal(0, 3)", class = "b"),
+              set_prior("normal(0, 4)", class = "sd"),
+              set_prior("normal(0, 3)", class = "sigma"),
+              set_prior("lkj(2)", class = "cor"))
+
+ld_rand_ind <- brm(ld_gaus_ind,
+                   data=indtree,
+                   control = list(adapt_delta = .8),
+                   prior = ld_prior)
 
 
+# These values are logit transformed... 
+library(boot)
+indtree$HERB <- inv.logit(indtree$HERB)
+indtree$HERB[indtree$HERB == 0] <- 1e-07
+
+ld_beta_ind <- bf(HERB ~ 1 + TREAT + (1 + TREAT|BLOCK) + (1 + TREAT|SP_CODE),
+                   family = Beta())
+
+ld_rand_ind <- brm(ld_beta_ind,
+                    data=indtree,
+                    control = list(adapt_delta = .8))
+
+stanplot(ld_rand_ind)
+
+pp = brms::pp_check(ld_rand_ind)
+pp + theme_bw()
+
+sph <- stanplot(ld_rand_ind, type = "hist", pars="^b_")
+
+#png("figs/ld_tree_diff_ind.png")
+
+X11(10,10)
+par(mfrow = c(3,2))
+
+# Change the name of this
+sph$data$Parameter
+
+for(type in unique(sph$data$Parameter)){
+  
+  vs <- sph$data$value[sph$data$Parameter == type]
+  
+  colour = "lightblue"
+  
+  if (mean(vs<0) > 0.90) colour = "orange"
+  if (mean(vs<0) > 0.95) colour = "red"
+  
+  hist(vs, breaks = 50,
+       main = paste(type, " ", round(mean(vs<0)*100,1), " %"),
+       xlab="Difference", col = colour)
+  
+  abline(v = 0, lty=2)
+  
+}
+#dev.off()
+
+indtree %>% 
+  tidybayes::add_predicted_draws(ld_rand_ind, n = 1000) %>%
+  ggplot(aes(x = logit(.prediction), y = TREAT, group = TREAT, fill=colour)) + 
+  geom_density_ridges(alpha = 0.5) + 
+  scale_fill_manual(values = c("grey", "red","orange")) +
+  #geom_point(aes(x = log(WEIGHT), y = TREAT), size = 3, alpha = 0.1,
+  #           color = "grey50") + 
+  theme_bw()
 
 # 2. SHANNON ------------------------------------------------------------------
 
@@ -457,74 +570,35 @@ AllTestData %>%
 # 3. SPECIES number ------------------------------------------------------------------- 
 
 sp_negb_f <- bf(SPEC_NO ~ 1 + TREAT + (1 + TREAT|GARDEN), family = "negbinomial")
+sp_pois_f <- bf(SPEC_NO ~ 1 + TREAT + (1 + TREAT|GARDEN), family = "poisson")
 
 # Set some priors
 
 # I DONT KNOW HOW TO SPECIFY THEM!!!
-sp_negbin_priors <- c(
-  set_prior("poisson(15)", class = "b"),
-  set_prior("lkj(1)", class = "cor"),
-  set_prior("poisson(15)", class = "Intercept"),
-  set_prior("normal(0,1)", class = "sd")
-)
+# sp_negbin_priors <- c(
+#   set_prior("poisson(15)", class = "b"),
+#   set_prior("lkj(1)", class = "cor"),
+#   set_prior("poisson(15)", class = "Intercept"),
+#   set_prior("normal(0,1)", class = "sd")
+# )
 
 # Prior predictive check!
-# sample from the prior distribution
-l3W_ranef_prior <- brm(sp_pois_f, data=AllTestData,
-           family = "negbinomial",
-           control = list(adapt_delta = 0.8),
-           prior = sp_no_priors, sample_prior = "only")
+# sample from the prior distribution -> # sample_prior = "only"
 
-l3W_ranef_no_prior <- brm(sp_pois_f, data=AllTestData,
-                       family = "negbinomial",
-                       control = list(adapt_delta = 0.8),
-                       file = "l3W_ranef_no_prior")
+l3W_ranef_negbin <- brm(sp_negb_f, data=AllTestData,
+                        family = "negbinomial",
+                        control = list(adapt_delta = 0.8))
 
+l3W_ranef_pois <- brm(sp_pois_f, data=AllTestData,
+                      family = "poisson",
+                      control = list(adapt_delta = 0.8))
 
-l3W_ranef_no_prior %>% summary
-
-
-####  3b. Plot ----------------------------------------------------
-
-pp = brms::pp_check(l3W_ranef_no_prior)
-pp + theme_bw()
-
-## for comparison another plot using predictions of marginal_effect plot
-
-
-# Pairwise comparisons
-
-sph <- stanplot(l3W_ranef_no_prior, type = "hist", pars="^b_")
-
-par(mfrow = c(3,2))
-
-# Change the name of this
-sph$data$Parameter
-
-for(type in unique(sph$data$Parameter)){
-  
-  vs <- sph$data$value[sph$data$Parameter == type]
-  
-  colour = "lightblue"
-  
-  if (mean(vs<0) > 0.90) colour = "orange"
-  if (mean(vs<0) > 0.95) colour = "red"
-  
-  hist(vs, breaks = 50,
-       main = paste(type, " ", round(mean(vs<0)*100,1), " %"),
-       xlab="Difference", col = colour)
-  
-  abline(v = 0, lty=2)
-  
-}
-
-
-
-# Plot template
+# I guess that Poisson stays?
+compare_ic(waic(l3W_ranef_pois), waic(l3W_ranef_negbin), ic = "waic")
 
 colour = as.character(AllTestData$TREAT)
 colour[colour == "CONTROL"] <- "grey50"
-colour[colour == "FUNGICIDE"] <- "grey50"
+colour[colour == "FUNGICIDE"] <- "orange"
 colour[colour == "INSECTICIDE"] <- "grey50"
 colour[colour == "PREDATOR"] <- "grey50"
 colour[colour == "WEEVIL25"] <- "grey50"
@@ -532,22 +606,24 @@ colour[colour == "WEEVIL125"] <- "grey50"
 
 AllTestData$colour = colour
 
+png("figs/rich_bayes_hist.png")
 AllTestData %>% 
-  tidybayes::add_predicted_draws(l3W_ranef_no_prior, n = 1000) %>%
+  tidybayes::add_predicted_draws(l3W_ranef_pois, n = 1000) %>%
   ggplot(aes(x = .prediction, y = TREAT, group = TREAT, fill=colour)) + 
   geom_density_ridges(alpha = 0.5) + 
   scale_fill_manual(values = c("grey", "orange", "red")) +
   geom_point(aes(x = SPEC_NO, y = TREAT), size = 3, alpha = 0.1,
              color = "grey50") + 
   theme_bw()
+dev.off()
 
-
-
-sph <- stanplot(l3W_ranef_prior, type = "hist", pars="^b_")
+png("figs/rich_all_diff.png")
 par(mfrow = c(3,2))
 
+sph <- stanplot(l3W_ranef_pois, type = "hist", pars="^b_")
 # Change the name of this
 sph$data$Parameter
+
 for(type in unique(sph$data$Parameter)){
   
   vs <- sph$data$value[sph$data$Parameter == type]
@@ -564,94 +640,10 @@ for(type in unique(sph$data$Parameter)){
   abline(v = 0, lty=2)
   
 }
-
-
-# visualize the prior predictive distribution:
-AllTestData %>% 
-  tidybayes::add_predicted_draws(l3W_ranef_no_prior) %>% #glimpse %>% 
-  filter(.draw ==3) %>% 
-  ggplot(aes(x = TREAT, y = .prediction)) + geom_point()
-
-# sample from the prior distribution
-l3W_ranef <- brm(sp_pois_f, data=AllTestData,
-                       family = poisson(),
-                       control = list(adapt_delta = 0.8),
-                       prior = sp_no_priors, sample_prior = "yes")
-
-
-# 1. Visualize fluffy plots
-AllTestData %>% 
-  tidybayes::add_predicted_draws(l3W_ranef_no_prior, n = 500) %>% glimpse %>% 
-  ggplot(aes(x = TREAT, y = SPEC_NO)) + 
-  geom_point(aes(y = .prediction), alpha = 0.1, position = position_jitter(width = 0.1)) + 
-  geom_point(colour = "green", size = 3) + 
-  facet_wrap(~GARDEN) + 
-  coord_flip() + 
-  theme_minimal()
-
-pp_check(l3W_ranef, fun = "stat_grouped")
-pp_check
-
-stanplot(l3W_ranef, type = "dens")
-
-launch_shinystan(l3W_ranef)
-
-# 2. Change the family
-
-
-# plot(l3W_ranef)
-
-pp = brms::pp_check(l3W_ranef)
-pp + theme_bw()
-
-sp_ranef_plot <- marginal_effects(l3W_ranef)
+dev.off()
 
 # Using predictions from the stanplots they produce the same results as predictions
-
 ## for comparison another plot using predictions of marginal_effect plot
-
-# Plot template
-ggplot(sp_ranef_plot$TREAT, aes(x = TREAT, y = estimate__)) +
-  geom_errorbar(aes(ymin=lower__, ymax=upper__), position=position_dodge(), size=1, width=.5) +
-  geom_jitter(data=AllTestData, aes(x=TREAT, y=SPEC_NO, group=GARDEN), 
-              alpha=0.10, size=4,
-              position = position_jitter(width = 0.07)) +
-  geom_line(data=AllTestData, aes(x=TREAT, y=SPEC_NO, 
-                                  group=GARDEN, 
-                                  linetype = GARDEN), 
-            size = 0.5, alpha=0.25) +
-  geom_point(shape=21, size=4, fill='red') +
-  xlab("") +
-  ylab("Shannon's Index") +
-  theme_bw () +
-  theme(panel.grid = element_blank())
-
-# Pairwise comparisons
-
-sph <- stanplot(l3W_ranef, type = "hist", pars="^b_")
-
-par(mfrow = c(3,2))
-
-# Change the name of this
-
-for(type in unique(sph$data$Parameter)){
-  
-  vs <- sph$data$value[sph$data$Parameter == type]
-  
-  colour = "lightblue"
-  
-  if (mean(vs<0) > 0.90) colour = "orange"
-  if (mean(vs<0) > 0.95) colour = "red"
-  
-  hist(vs, breaks = 50,
-       main = paste(type, " ", round(mean(vs<0)*100,1), " %"),
-       xlab="Difference", col = colour)
-  
-  abline(v = 0, lty=2)
-  
-}
-
-stanplot(l3W_ranef, type = "hist",  pars="^b_")
 
 
 # 4. Evenness -------------------------------------------------------------------
@@ -666,45 +658,14 @@ l4W_ranef <- brm(ev_norm_f, data=AllTestData,
            control = list(adapt_delta = 0.8), file = "l4W_ranef")
 
 
-# 4b. Plots --------------------
-
 pp = brms::pp_check(l4W_ranef)
 pp + theme_bw()
 
 
-colour = as.character(AllTestData$TREAT)
-colour[colour == "CONTROL"] <- "grey50"
-colour[colour == "FUNGICIDE"] <- "grey50"
-colour[colour == "INSECTICIDE"] <- "orange"
-colour[colour == "PREDATOR"] <- "grey50"
-colour[colour == "WEEVIL25"] <- "orange"
-colour[colour == "WEEVIL125"] <- "grey50"
-
-AllTestData$colour = colour
-
-AllTestData %>% 
-  tidybayes::add_predicted_draws(l4W_ranef, n = 1000) %>%
-  ggplot(aes(x = .prediction, y = TREAT, group = TREAT, fill=colour)) + 
-  geom_density_ridges(alpha = 0.5) + 
-  scale_fill_manual(values = c("grey", "orange", "red")) +
-  geom_point(aes(x = EVEN, y = TREAT), size = 3, alpha = 0.1,
-             color = "grey50") + 
-  theme_bw()
-
-
-
-# Using predictions from the stanplots they produce the same results as predictions
-
-## for comparison another plot using predictions of marginal_effect plot
-
-# Pairwise comparisons
-
 sph <- stanplot(l4W_ranef, type = "hist", pars="^b_")
-
+png("figs/eve_all_diff.png")
+#X11(10,10)
 par(mfrow = c(3,2))
-
-# Change the name of this
-
 for(type in unique(sph$data$Parameter)){
   
   vs <- sph$data$value[sph$data$Parameter == type]
@@ -721,64 +682,119 @@ for(type in unique(sph$data$Parameter)){
   abline(v = 0, lty=2)
   
 }
+dev.off()
 
 
- 
+colour = as.character(AllTestData$TREAT)
+colour[colour == "CONTROL"] <- "grey50"
+colour[colour == "FUNGICIDE"] <- "grey50"
+colour[colour == "INSECTICIDE"] <- "orange"
+colour[colour == "PREDATOR"] <- "grey50"
+colour[colour == "WEEVIL25"] <- "orange"
+colour[colour == "WEEVIL125"] <- "grey50"
 
-# Tree Biomass ---------------------------------------------------------------
+AllTestData$colour = colour
+
+png("figs/eve_bayes_hist.png")
+AllTestData %>% 
+  tidybayes::add_predicted_draws(l4W_ranef, n = 1000) %>%
+  ggplot(aes(x = .prediction, y = TREAT, group = TREAT, fill=colour)) + 
+  geom_density_ridges(alpha = 0.5) + 
+  scale_fill_manual(values = c("grey", "orange", "red")) +
+  geom_point(aes(x = EVEN, y = TREAT), size = 3, alpha = 0.1,
+             color = "grey50") + 
+  theme_bw()
+dev.off()
+
+
+
+# 5. Tree Biomass ---------------------------------------------------------------
+
+TreeTestDataset$TREATMENT
+
 
 # Specify priors
 bio_norm_tree <- bf(logBio ~ 1 + TREATMENT + (1 + TREATMENT|GARDEN), family = gaussian())
-bio_tree_prior <- get_prior(bio_norm_tree, data = TreeTestDataset)
 
 # Model
 l1Wt <- brm(bio_norm_tree, 
             data=TreeTestDataset,
             control = list(adapt_delta = 0.99),
-            prior = bio_tree_prior,
             file="bioTre")
 
+pp = brms::pp_check(l1Wt)
+pp + theme_bw()
 
-bio_ranef_tree <- marginal_effects(l1Wt)
+
+sph <- stanplot(l1Wt, type = "hist", pars="^b_")
+png("figs/bio_tree_diff.png")
+#X11(10,10)
+par(mfrow = c(3,2))
+for(type in unique(sph$data$Parameter)){
+  
+  vs <- sph$data$value[sph$data$Parameter == type]
+  
+  colour = "lightblue"
+  
+  if (mean(vs<0) > 0.90) colour = "orange"
+  if (mean(vs<0) > 0.95) colour = "red"
+  
+  hist(vs, breaks = 50,
+       main = paste(type, " ", round(mean(vs<0)*100,1), " %"),
+       xlab="Difference", col = colour)
+  
+  abline(v = 0, lty=2)
+  
+}
+dev.off()
+
+
+colour = as.character(TreeTestDataset$TREAT)
+colour[colour == "CONTROL"] <- "grey50"
+colour[colour == "FUNGICIDE"] <- "grey50"
+colour[colour == "INSECTICIDE"] <- "grey50"
+colour[colour == "PREDATOR"] <- "grey50"
+colour[colour == "WEEVIL25"] <- "grey50"
+colour[colour == "WEEVIL125"] <- "red"
+
+TreeTestDataset$colour = colour
+
+png("figs/bio_bayes_tree_hist.png")
+TreeTestDataset %>% 
+  tidybayes::add_predicted_draws(l1Wt, n = 1000) %>%
+  ggplot(aes(x = .prediction, y = TREATMENT, group = TREATMENT, fill=colour)) + 
+  geom_density_ridges(alpha = 0.5) + 
+  scale_fill_manual(values = c("grey", "red","orange")) +
+  geom_point(aes(x = logBio, y = TREATMENT), size = 3, alpha = 0.1,
+             color = "grey50") + 
+  theme_bw()
+dev.off()
 
 ## for comparison another plot using predictions of marginal_effect plot
 # Plot template
 
-p <- ggplot(bio_ranef_tree$TREAT, aes(x = TREATMENT, y = estimate__)) +
-  geom_errorbar(aes(ymin=lower__, ymax=upper__), position=position_dodge(), size=1, width=.5) +
-  geom_jitter(data=TreeTestDataset, aes(x=TREATMENT, y=logBio, group=GARDEN), 
-              alpha=0.10, size=4,
-              position = position_jitter(width = 0.07)) +
-  geom_line(data=TreeTestDataset, aes(x=TREATMENT, y=logBio, 
-                                  group=GARDEN, 
-                                  linetype = GARDEN), 
-            size = 0.5, alpha=0.25) +
-  geom_point(shape=21, size=4, fill='red') +
-  xlab("") +
-  ylab("LN(BIOMASS)") +
-  theme_bw () +
-  theme(panel.grid = element_blank())
 
-p
-
-# plotMyResults(l1Wt)
-
-# Tree Diversity --------------------------------------------------------------
+# 6. Tree Diversity --------------------------------------------------------------
 
 # l2Wt <- brm(DivTree~TREATMENT + (1|GARDEN), data=TreeTestDataset,
 #             control = list(adapt_delta = 0.99),
 #             file="swiTre")
-# 
+
+# 7. Richness ---------------------------------
 # l3Wt <- brm(species~TREATMENT+(1|GARDEN), data=TreeTestDataset,
 #               family = poisson(),
 #               control = list(adapt_delta = 0.99),
 #               file="nosTre")
-# 
+
+
+# 8. Evenness ---------------
 # l4Wt <- brm(Evenness~TREATMENT + (1|GARDEN), data=TreeTestDataset,
 #              family = Beta(),
 #              control = list(adapt_delta = 0.99),
 #              file = "eveTre")
-# 
+
+# 9. Stem number -----------------
+
 # l5Wt <- brm(stems~TREATMENT + (1|GARDEN), data=TreeTestDataset,
 #             family=poisson(),
 #             control = list(adapt_delta = 0.9999),
@@ -839,3 +855,17 @@ plotMyResults <- function(l2W_ranef){
   p
   
 }
+
+### Plots in a panel -------------------------- UNDER CONSTRUCTION
+
+#### Hold the plot
+BD <- AllTestData %>% 
+  tidybayes::add_predicted_draws(l1W_ranef, n = 1000)
+
+BDp <- ggplot(BD,  aes(x = .prediction, y = TREAT, group = TREAT, fill=colour)) + 
+  geom_density_ridges(alpha = 0.5) + 
+  scale_fill_manual(values = c("grey", "orange", "red")) +
+  geom_point(aes(x = log(BIO), y = TREAT), size = 3, alpha = 0.1,
+             color = "grey50") + 
+  theme_bw()
+####
