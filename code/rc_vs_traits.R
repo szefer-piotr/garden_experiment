66440b222c5e8c38862d6fb1ec3243424c6c7e4f
Piotr Szefer <szefer85@gmail.com>
2019-11-17 12:20:59 +0100

    :script: revision cd

---
+++ code/rc_vs_traits.R
@@ -0,0 +1,23 @@
+write.table(dataRC_facets, "datasets/dataRC")
+read.table("datasets/dataRC.txt")
+
+rcdat <- dataRC_facets[49:60,] # insecticide
+main
+sladat <- cwmpanel[cwmpanel$type == "Specific Leaf Area" & cwmpanel$treat %in% c("C","I"), ]
+sladat <- cwmpanel[cwmpanel$type == "Leaf Dry Matter Content" & cwmpanel$treat %in% c("C","I"), ]
+
+rcdat$plot <- as.character(rcdat$plot)
+sladat$plot <- rownames(sladat)
+
+rcdat$plot == sladat$plot
+rcdat <- rcdat[order(rcdat$plot),]
+sladat <- sladat[order(sladat$plot),]
+
+rcdat <- rcdat[rcdat$plot != "WG1P6", ]
+
+cordat <- cbind(rcdat, sladat)
+cordat[, c(2,6)]
+
+x11(4,4)
+plot(meanRC~val, data=cordat)
+summary(lm(meanRC~val, data=cordat))
